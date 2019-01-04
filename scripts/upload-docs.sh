251325460fdbfcf34dac50da828f2db474c552a0
Chris D'Aloisio <chris@creativecontrol.com.au>
2019-01-04 23:35:49 +1100

    Tweak haddock generate options

--- scripts/upload-docs.sh
+++ scripts/upload-docs.sh
@@ -4,5 +4,5 @@ set -e
 dir=$(mktemp -d dist-docs.XXXXXX)
 trap 'rm -r "$dir"' EXIT
 
-cabal new-haddock --builddir="$dir" --for-hackage --haddock-option=--hyperlinked-source
+cabal new-haddock --builddir="$dir" --haddock-for-hackage --haddock-option=--hyperlinked-source
 cabal upload -d $dir/*-docs.tar.gz
