9dde75d9ddcb7292b416d8d56191bbfb86ddd28b
Chris D'Aloisio <chris@creativecontrol.com.au>
2019-01-31 23:13:34 +1100

    Added example flag

--- scripts/upload-docs.sh
+++ scripts/upload-docs.sh
@@ -4,5 +4,5 @@ set -e
 dir=$(mktemp -d dist-docs.XXXXXX)
 trap 'rm -fr "$dir"' EXIT
 
-cabal haddock --builddir="$dir" --for-hackage --haddock-option=--hyperlinked-source
+cabal new-haddock --builddir="$dir" --haddock-for-hackage --haddock-option=--hyperlinked-source
 cabal upload -d $dir/*-docs.tar.gz
